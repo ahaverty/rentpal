c6d9e97ed16711e77fb677318e86bf5a27ff01d7
Alan Haverty <alan@ahaverty.com>
2015-05-03 12:58:14 +0100

    Minor formatting made to article template.

--- templates/pages/home/article_template.php
+++ templates/pages/home/article_template.php
@@ -3,30 +3,32 @@
 		<i class="glyphicon glyphicon-comment"></i>
 	</div>
 	<div class="panel-heading">
-	
-		<div class="panel-actions">	
+
+		<div class="panel-actions">
+		
 			<!-- Edit Record button -->
 			<form action="#" method="post">
-				<input type='hidden' name='action' value='edit-record'>
-				<input type='hidden' name='record_id' value='{{ recordId }}'>
+				<input type='hidden' name='action' value='edit-record'> <input
+					type='hidden' name='record_id' value='{{ recordId }}'>
 				<button type="submit" class="btn btn-info btn-xs">
 					<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
 				</button>
 			</form>
+			
 			<!-- Delete Record button -->
 			<form id="delete-record" action="#" method="post">
-				<input type='hidden' name='action' value='deleteRecord'>
-				<input type='hidden' name='record_id' value='{{ recordId }}'>
-				<button type="submit" class="btn btn-danger btn-xs" onclick="return confirm('Are you sure you want to delete this record?');">
+				<input type='hidden' name='action' value='deleteRecord'> <input
+					type='hidden' name='record_id' value='{{ recordId }}'>
+				<button type="submit" class="btn btn-danger btn-xs"
+					onclick="return confirm('Are you sure you want to delete this record?');">
 					<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
 				</button>
 			</form>
+			
 		</div>
 		<div class="panel-heading-text">
 			<h2 class="panel-title">{{ header }}</h2>
 		</div>
 	</div>
-	<div class="panel-body">
-		{{ body }}
-	</div>
+	<div class="panel-body">{{ body }}</div>
 </article>
